d3da5b3e6b37f91ecaa3bc43b7f2105400cd1e95
Mike Mackenzie <mike@veb.co.nz>
2021-03-28 19:20:37 +1300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,12 @@
-import profile from "./profile.jpg";
+import React, { Component } from "react";
+import LinkList from "./LinkList";
 
-function App() {
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={profile} alt="Display" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Blessing Krofegha
-        </div>
-        <p className="text-gray-700 text-base">
-          When i’m not coding i switch to netflix with biscuits and cold tea as
-          my companion. <span></span>😜
-        </p>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #Software Engineer
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #Writter
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 ml-20">
-          #Public Speaker
-        </span>
-      </div>
-    </div>
-  );
+import { Flex, Spacer } from "@chakra-ui/react";
+
+class App extends Component {
+  render() {
+    return <LinkList />;
+  }
 }
+
 export default App;
