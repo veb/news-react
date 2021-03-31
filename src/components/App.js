92c693ebce30f84258d72af658dbf77ad9341017
Mike Mackenzie <mike@veb.co.nz>
2021-03-31 16:19:11 +1300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
+// import LinkList from "./LinkList";
 import LinkList from "./LinkList";
-
-import { Flex, Spacer } from "@chakra-ui/react";
+import { Container, Button } from "react-bootstrap";
+import { FaThumbsUp } from "react-icons/fa";
+import Logo from "images/logo.png";
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    return (
+      <React.Fragment>
+        <div class="topbar"></div>
+        <div class="container">
+          <div class="topbar"></div>
+          <div class="page-header">
+            <a class="btn btn-lg pull-right" href="/add/">
+              <Button variant="primary">
+                <FaThumbsUp /> Ask Question
+              </Button>
+            </a>
+            <a href="/">
+              <img width="200px" src={Logo} />
+            </a>
+          </div>
+          <LinkList />
+          <hr />
+          <center>
+            <a href="mialto:sup@dankoverflow.com">Contact</a>
+          </center>
+          <br />
+          <br />
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
