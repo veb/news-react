6d549772da62cc76b1a8b98ac7a500259b778d32
Mike Mackenzie <mike@veb.co.nz>
2021-04-06 14:55:26 +1200

    Create CreateLink.js

---
+++ src/components/CreateLink.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+
+const CREATE_LINK_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+const CreateLink = () => {
+  const [formState, setFormState] = useState({
+    description: "",
+    url: "",
+  });
+
+  const [createLink] = useMutation(CREATE_LINK_MUTATION, {
+    variables: {
+      description: formState.description,
+      url: formState.url,
+    },
+  });
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createLink();
+        }}
+      >
+        <div>
+          <input
+            className="formDescription"
+            value={formState.description}
+            onChange={(e) => {
+              setFormState({
+                ...formState,
+                description: e.target.value,
+              });
+            }}
+            type="text"
+            placeholder="A new description for the link"
+          />
+          <input
+            className="formUrl"
+            value={formState.url}
+            onChange={(e) => {
+              setFormState({
+                ...formState,
+                url: e.target.value,
+              });
+            }}
+            type="text"
+            placeholder="The URL"
+          />
+        </div>
+        <button type="submit">Submit Link</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateLink;
