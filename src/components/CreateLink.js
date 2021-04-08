f87bb55bb557759b3cc796c24c68adfbd6fb4ad5
Mike Mackenzie <mike@veb.co.nz>
2021-04-09 09:47:40 +1200

    bug. removed.

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -5,7 +5,7 @@ const CREATE_LINK_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
     post(description: $description, url: $url) {
       id
-      createdAt
+      # createdAt
       url
       description
     }
